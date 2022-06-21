5f9a93383ec8331442f397cd6dbbac4bf08738d3
yukiyamamuro <yuki.sumaho@gmail.com>
2022-06-21 12:06:43 +0900

    chore: save

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
-import { Box, Button, Card, Container, CssBaseline, Grid, Input, Typography } from '@mui/material';
 import { FC, useEffect, useState } from 'react';
+import { Box, Button, Card, Container, CssBaseline, Grid, Input, Typography } from '@mui/material';
+import { Commitment ,Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { AnchorProvider, Program, web3 } from '@project-serum/anchor';
+
 import { Solana } from './types/Solana';
+import idl from './idl.json';
 
 declare global {
   interface Window {
     solana: Solana;
   }
+}
+
+
+const { SystemProgram, Keypair } = web3;
+let baseAccount = Keypair.generate();
+const com: Commitment = 'processed'
+const programID = new PublicKey(idl.metadata.address);
+const network = clusterApiUrl('devnet');
+const opts = {
+  preflightCommitment: com,
 }
 
 const TEST_GIFS = [
@@ -15,7 +29,7 @@ const TEST_GIFS = [
 ]
 
 export const App: FC = () => {
-  const [walletAddres, setWalletAddress] = useState<string| null>(null);
+  const [walletAddress, setWalletAddress] = useState<string| null>(null);
   const [inputValue, setInputValue] = useState<string>('');
   const [gifList, setGifList] = useState<string[]>([]);
 
@@ -56,13 +70,54 @@ export const App: FC = () => {
     setInputValue(value);
   };
 
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new AnchorProvider(
+      connection, window.solana, opts.preflightCommitment,
+    );
+    return provider;
+  }
+
+  const createGifAccount = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      console.log("ping")
+      await program.rpc.startStuffOff({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount]
+      });
+      console.log("Created a new BaseAccount w/ address:", baseAccount.publicKey.toString())
+      await getGifList();
+
+    } catch(error) {
+      console.log("Error creating BaseAccount account:", error)
+    }
+  }
   const sendGif = async () => {
-    if (inputValue.length > 0) {
-      console.log('Gif link:', inputValue);
-      setGifList([...gifList, inputValue]);
-      setInputValue('');
-    } else {
-      console.log('Empty input. Try again.');
+    if (inputValue.length === 0) {
+      console.log("No gif link given!")
+      return
+    }
+    setInputValue('');
+    console.log('Gif link:', inputValue);
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      await program.rpc.addGif(inputValue, {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+        },
+      });
+      console.log("GIF successfully sent to program", inputValue)
+      await getGifList();
+    } catch (error) {
+      console.log("Error sending GIF:", error)
     }
   };
 
@@ -74,13 +129,26 @@ export const App: FC = () => {
     return () => window.removeEventListener('load', onLoading);
   }, []);
 
-  useEffect(() => {
-    if (walletAddres) {
-      console.log('Fetching gifs...');
+  const getGifList = async() => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
 
-      setGifList(TEST_GIFS);
+      console.log("Got the account", account)
+      setGifList(account.gifList)
+    } catch (error) {
+      console.log("Error in getGifList: ", error)
+      setGifList(null);
     }
-  }, [walletAddres]);
+  }
+
+  useEffect(() => {
+    if (walletAddress) {
+      console.log('Fetching GIF list...');
+      getGifList()
+    }
+  }, [walletAddress]);
 
   return (
     <Box
@@ -100,7 +168,7 @@ export const App: FC = () => {
         </Typography>
         <Typography variant="body1" paddingY={5}>connect your phantom wallet!!</Typography>
         {
-          !walletAddres ?
+          !walletAddress ?
           (<Button
             onClick={connectWallet}
             variant="contained"
@@ -114,28 +182,37 @@ export const App: FC = () => {
           </Button>) : null
         }
         <main className='main'>
-        {walletAddres && (
-          <>
-            <Card sx={{ padding: 4 }}>
-              <form onSubmit={(e) => {e.preventDefault(); sendGif();}}>
-                <Typography>Enter GIF link you want to add</Typography>
-                <Input
-                  type='text'
-                  placeholder='Gif link!'
-                  sx={{ marginY: 3 }}
-                  fullWidth
-                  value={inputValue}
-                  onChange={onInputChange}
-                />
-                <Button type='submit' variant="contained" size='large' fullWidth>Submit</Button>
-              </form>
-            </Card>
-            {gifList.map(gif => (
-                <Grid key={gif} sx={{ padding: 1 }} >
-                  <img src={gif} alt="gif"  height={250} width={250} />
-                </Grid>
-            ))}
-          </>)
+        {walletAddress && (
+          gifList === null ? (
+              <div className="connected-container">
+                <button className="cta-button submit-gif-button" onClick={createGifAccount}>
+                  Do One-Time Initialization For GIF Program Account
+                </button>
+              </div>
+            )
+          : (
+            <>
+              <Card sx={{ padding: 4 }}>
+                <form onSubmit={(e) => {e.preventDefault(); sendGif();}}>
+                  <Typography>Enter GIF link you want to add</Typography>
+                  <Input
+                    type='text'
+                    placeholder='Gif link!'
+                    sx={{ marginY: 3 }}
+                    fullWidth
+                    value={inputValue}
+                    onChange={onInputChange}
+                  />
+                  <Button type='submit' variant="contained" size='large' fullWidth>Submit</Button>
+                </form>
+              </Card>
+              {gifList.map((item, index) => (
+                  <Grid key={index} sx={{ padding: 1 }} >
+                    <img src={item.gifLink} alt="gif"  height={250} width={250} />
+                  </Grid>
+              ))}
+            </>)
+            )
           }
         </main>
       </Container>
--- frontend/src/types/Solana.d.ts
+++ frontend/src/types/Solana.d.ts
@@ -1,5 +1,8 @@
 
 export interface Solana {
   isPhantom?: boolean;
+  publicKey: PublicKey;
   connect: (a?: any) => Promise<any>;
+  signTransaction: (a?: any) => Promise<any>;
+  signAllTransactions: (a?: any) => Promise<any>;
 }
